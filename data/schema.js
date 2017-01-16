041dcc6cc8cfb4cb6ee8b5e41b8bdd25b26c68c8
Benjamin Chodoroff <bc@thermitic.net>
2017-01-16 15:04:47 -0500

    tab->spaces

--- data/schema.js
+++ data/schema.js
@@ -1,36 +1,36 @@
 const typeDefinitions = [`
 type IrsOrganization {
-	id: Int
-	ein: String
-	subsccd: String
-	pdf: String
-	filing_type: String
-	start_year: Int
-	end_year: Int
-	irs_year: Int
-	filing_date: String
-	tax_period: String
-	contributions_and_grants: Int
-	program_service_revenue: Int
-	investment_income: Int
-	other_revenue: Int
-	total_revenue: Int
-	grants_paid: Int
-	benefits_paid: Int
-	compensation: Int
-	fundraising_fees: Int
-	total_fundraising_expenses: Int
-	other_expenses: Int
-	total_expenses: Int
-	revenue_less_expenses: Int
-	total_assets: Int
-	total_liabilities: Int
-	net_assets: Int
-	data: String
-	forms990(limit: Int = 5, offset: Int = 0): [Form990]
+  id: Int
+  ein: String
+  subsccd: String
+  pdf: String
+  filing_type: String
+  start_year: Int
+  end_year: Int
+  irs_year: Int
+  filing_date: String
+  tax_period: String
+  contributions_and_grants: Int
+  program_service_revenue: Int
+  investment_income: Int
+  other_revenue: Int
+  total_revenue: Int
+  grants_paid: Int
+  benefits_paid: Int
+  compensation: Int
+  fundraising_fees: Int
+  total_fundraising_expenses: Int
+  other_expenses: Int
+  total_expenses: Int
+  revenue_less_expenses: Int
+  total_assets: Int
+  total_liabilities: Int
+  net_assets: Int
+  data: String
+  forms990(limit: Int = 5, offset: Int = 0): [Form990]
   ledgerOrganizations(limit: Int = 5, offset: Int = 0): [LedgerOrganization]
-	ledgerGrants(limit: Int = 5, offset: Int = 0): [LedgerGrant]
-	ledgerNewsArticles(limit: Int = 5, offset: Int = 0): [LedgerNewsArticle]
+  ledgerGrants(limit: Int = 5, offset: Int = 0): [LedgerGrant]
+  ledgerNewsArticles(limit: Int = 5, offset: Int = 0): [LedgerNewsArticle]
 }
 
 type Form990 {
