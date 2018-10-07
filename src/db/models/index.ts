2eeec0dea1bf67b3fa8d142a077215d44ad46f26
Benjamin Chodoroff <bc@thermitic.net>
2018-10-07 11:18:06 -0400

    add csv-parse depend
    try to make typing work with vim tsuquyomi

--- src/db/models/index.ts
+++ src/db/models/index.ts
@@ -4,35 +4,50 @@ import * as Sequelize from 'sequelize';
 import { logger as baseLogger } from '../../logger';
 
 import form990Factory from './form990';
-import grantTagFactory from './grantTag';
-import organizationTagFactory from './organizationTag';
-import nteeGrantTypeFactory from './nteeGrantType';
-import nteeOrganizationTypeFactory from './nteeOrganizationType';
-import grantFactory from './grant';
-import organizationFactory from './organization';
+import grantTagFactory, * as grantTag from './grantTag';
+import organizationTagFactory, * as organizationTag from './organizationTag';
+import nteeGrantTypeFactory, * as nteeGrantType from './nteeGrantType';
+import nteeOrganizationTypeFactory, * as nteeOrganizationType from './nteeOrganizationType';
+import grantFactory, * as grant from './grant';
+import organizationFactory, * as organization from './organization';
 
-const logger = baseLogger.child({module: 'database'});
-const dbConfig = config.get('database') as Sequelize.Options;
-dbConfig.logging = (msg) => logger.debug(msg);
-logger.info('setting up database with dialect %s', dbConfig.dialect);
-
-const sequelize = new Sequelize(config.get('database'));
-
-const db = {
-  sequelize,
-  Sequelize,
-  GrantTag: grantTagFactory(sequelize),
-  organizationTag: organizationTagFactory(sequelize),
-  nteeGrantType: nteeGrantTypeFactory(sequelize),
-  NteeOrganizationType: nteeOrganizationTypeFactory(sequelize),
-  Grant: grantFactory(sequelize),
-  Organization: organizationFactory(sequelize),
+export interface Db {
+  sequelize: Sequelize.Sequelize
+  Sequelize: Sequelize.SequelizeStatic
+  GrantTag: Sequelize.Model<grantTag.GrantTagInstance, grantTag.GrantTagAttributes>
+  OrganizationTag: Sequelize.Model<organizationTag.OrganizationTagInstance, organizationTag.OrganizationTagAttributes>
+  NteeGrantType: Sequelize.Model<nteeGrantType.NteeGrantTypeInstance, nteeGrantType.NteeGrantTypeAttributes>
+  NteeOrganizationType: Sequelize.Model<nteeOrganizationType.NteeOrganizationTypeInstance, nteeOrganizationType.NteeOrganizationTypeAttributes>
+  Grant: Sequelize.Model<grant.GrantInstance, grant.GrantAttributes>
+  Organization: Sequelize.Model<organization.OrganizationInstance, organization.OrganizationAttributes>
 };
 
-Object.values(db.sequelize.models).forEach((model: Sequelize.Model<any, any>) => {
-  if (model.associate) {
-    model.associate(db.sequelize.models);
-  }
-});
+export default function dbFactory(): Db {
+  const logger = baseLogger.child({module: 'database'});
+  const dbConfig = config.get('database') as Sequelize.Options;
 
-export default db;
+  dbConfig.logging = (msg) => logger.debug(msg);
+
+  logger.info('setting up database with dialect %s', dbConfig.dialect);
+
+  const sequelize = new Sequelize(config.get('database'));
+
+  const db: Db = {
+    sequelize,
+    Sequelize,
+    GrantTag: grantTagFactory(sequelize),
+    OrganizationTag: organizationTagFactory(sequelize),
+    NteeGrantType: nteeGrantTypeFactory(sequelize),
+    NteeOrganizationType: nteeOrganizationTypeFactory(sequelize),
+    Grant: grantFactory(sequelize),
+    Organization: organizationFactory(sequelize),
+  };
+
+  Object.values(db.sequelize.models).forEach((model: Sequelize.Model<any, any>) => {
+    if (model.associate) {
+      model.associate(db.sequelize.models);
+    }
+  });
+
+  return db;
+}
