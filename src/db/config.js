ed694a46afcd13d86b2a6db579203d47e93c6537
Benjamin Chodoroff <bc@thermitic.net>
2018-12-19 09:39:02 -0500

    oops! change user to username

--- src/db/config.js
+++ src/db/config.js
@@ -1,6 +1,5 @@
 const config = require('config');
 
-console.warn({ dburl: process.env.DATABASE_URL });
 // The config module determines which config files to load based on NODE_ENV
 module.exports = {
   development: config.get('database'),
