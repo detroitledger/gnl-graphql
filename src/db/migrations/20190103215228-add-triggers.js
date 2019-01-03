01f63701e51576f8880f7a0e093dee2a74428290
Benjamin Chodoroff <bc@thermitic.net>
2019-01-03 17:19:46 -0500

    add trigger to update mat view
    fixes #25

---
+++ src/db/migrations/20190103215228-add-triggers.js
@@ -0,0 +1,53 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize
+      .query(
+        `
+create unique index organization_meta_id on organization_meta (id)
+`
+      )
+      .then(() => {
+        queryInterface.sequelize.query(`
+create or replace function refresh_organization_meta()
+returns trigger language plpgsql
+as $$
+begin
+    refresh materialized view concurrently organization_meta;
+    return null;
+end $$;
+`);
+      })
+      .then(() => {
+        queryInterface.sequelize.query(`
+create trigger changed_grant
+after insert or update or delete or truncate
+on "grant" for each statement 
+execute procedure refresh_organization_meta();
+`);
+      })
+      .then(() => {
+        queryInterface.sequelize.query(`
+create trigger changed_organization
+after insert or update or delete or truncate
+on organization for each statement 
+execute procedure refresh_organization_meta();
+`);
+      });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize
+      .query(
+        `
+drop index if exists organization_meta_id
+`
+      )
+      .then(() => {
+        queryInterface.sequelize.query(`
+drop function if exists refresh_organization_meta() cascade
+`);
+      });
+  },
+};
